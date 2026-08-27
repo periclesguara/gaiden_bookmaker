f7885ca07362f5265a3214614cdd3ae0e7f45afe
periclesguara <periclesguara@gmail.com>
2026-08-27 15:35:06 -0300

    feat(storefront): register generic sales channels field

--- web/editorial/apps.py
+++ web/editorial/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class EditorialConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'editorial'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "editorial"
+
+    def ready(self):
+        from editorial.storefront_availability import install_sales_channels_field
+
+        install_sales_channels_field()
